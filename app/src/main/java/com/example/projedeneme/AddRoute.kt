5d95736c0e7c4472196cd283f4af8307ddf538a5
strafor <aawifoa@gmail.com>
2020-08-18 18:02:46 +0300

    Add Route Eklendi

---
+++ app/src/main/java/com/example/projedeneme/AddRoute.kt
@@ -0,0 +1,11 @@
+package com.example.projedeneme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddRoute : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_route)
+    }
+}
