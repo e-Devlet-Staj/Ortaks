147394ade72bfdcf005beebcd7efe3945d1bc740
strafor <aawifoa@gmail.com>
2020-08-18 18:00:49 +0300

    Add Route eklendi
    add route eklendi ve küçük değişiklikler yapıldı

--- app/src/main/java/com/example/projedeneme/Deneme.kt
+++ app/src/main/java/com/example/projedeneme/Deneme.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class Deneme : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_deneme)
+        setContentView(R.layout.activity_main)
     }
 }
