dbf5e3b134c8121936c2c541b3575bd94690f79a
strafor <aawifoa@gmail.com>
2020-08-20 10:41:53 +0300

    Map Açıldığında mevcut konuma pin koyuldu.

--- app/src/main/java/com/example/projedeneme/AddRouteMap.kt
+++ app/src/main/java/com/example/projedeneme/AddRouteMap.kt
@@ -1,9 +1,19 @@
 package com.example.projedeneme
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,7 +26,10 @@ import kotlinx.android.synthetic.main.activity_add_route_map.*
 class AddRouteMap : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    val PERMISSION_ID = 1010
+    var options = MarkerOptions()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_route_map)
@@ -24,36 +37,27 @@ class AddRouteMap : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        Log.d("Debug:",CheckPermission().toString())
+        Log.d("Debug:",isLocationEnabled().toString())
+        RequestPermission()
+        getLastLocation()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
+        
         buttonSearch.setOnClickListener {
-            var location=editTextMapSearchBar.text.toString()
-            var addressList : List<Address>?=null
-            var options = MarkerOptions()
-            if(location!=""){
-                var geocoder =Geocoder(this)
+            var location = editTextMapSearchBar.text.toString()
+            var addressList: List<Address>? = null
 
-                addressList= geocoder.getFromLocationName(location,5)
-                for(i in addressList!!.indices){
+            if (location != "") {
+                var geocoder = Geocoder(this)
+
+                addressList = geocoder.getFromLocationName(location, 5)
+                for (i in addressList!!.indices) {
                     var address = addressList[i]
-                    var latLng=LatLng(address.latitude,address.longitude)
+                    var latLng = LatLng(address.latitude, address.longitude)
                     options.position(latLng)
                     options.title("Aranan Konum")
                     mMap!!.addMarker(options)
@@ -62,5 +66,128 @@ class AddRouteMap : AppCompatActivity(), OnMapReadyCallback {
 
             }
         }
+    }
+
+    private fun CheckPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+            return true
+        else
+            return false
+    }
+
+    private fun RequestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ), PERMISSION_ID
+        )
+    }
+
+    fun isLocationEnabled(): Boolean {
+        //this function will return to us the state of the location service
+        //if the gps or the network provider is enabled then it will return true otherwise it will return false
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Debug:", "You have the Permission")
+            }
+        }
+    }
+
+    fun getLastLocation() {
+        if (CheckPermission()) {
+            if (isLocationEnabled()) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        NewLocationData()
+                    } else {
+                        Log.d("Debug:", "Your Location:" + location.longitude)
+                        var latLng = LatLng(location.latitude, location.longitude)
+                        options.position(latLng)
+                        options.title("Aranan Konum")
+                        mMap!!.addMarker(options)
+                        mMap!!.animateCamera(CameraUpdateFactory.newLatLng(latLng)) }
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please Turn on Your device Location", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+    fun NewLocationData(){
+        var locationRequest =  LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 1
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationProviderClient!!.requestLocationUpdates(
+            locationRequest,locationCallback, Looper.myLooper()
+        )
+    }
+
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+
+            var lastLocation: Location = locationResult.lastLocation
+            Log.d("Debug:","your last last location: "+ lastLocation.longitude.toString())
+            }
     }
 }
